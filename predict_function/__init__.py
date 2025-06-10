fbe976772f209e945060144c32cfef8b792d03ea
bpdev75 <brice.poussin@gmail.com>
2025-06-10 11:49:17 +0200

    Test deployment

--- predict_function/__init__.py
+++ predict_function/__init__.py
@@ -5,7 +5,7 @@ import pickle
 import numpy as np
 import pandas as pd
 import logging
-from model.hybrid import HybridArticleRecommender
+#from model.hybrid import HybridArticleRecommender
 
 logging.basicConfig(level=logging.INFO)
 
