6d86af1e3a71835df83c64e377cce2707baca7ea
bpdev75 <brice.poussin@gmail.com>
2025-06-15 00:12:55 +0200

    Restore previous structure

--- function_app.py
+++ function_app.py
@@ -20,7 +20,8 @@ train_df = pd.read_csv(os.path.join(data_path, "clicks_train.csv"))
 test_df = pd.read_csv(os.path.join(data_path, "clicks_test.csv"))
 hybrid_model = HybridArticleRecommender(train_df, test_df, article_embeddings, alpha=0.5)
 
-@app.route(route="predict_function")
+@app.function_name(name="predict_function")
+@app.route(route="predict")
 def predict_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
